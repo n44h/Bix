e22afed7675009f1bf37c8d23c26b8b56b90d62d
Nadeem <62071086+CookieCrumbs19212@users.noreply.github.com>
2021-11-03 03:28:54 +0530

    Added config.properties parsing functionality.
    Renamed constants and variables.
    
    Minor refactoring for clarity.
    
    Added integration for enum AES_FLAVORS.
    
    Added setup() method for initial setup of Bix.
    
    Added SetMasterPassword() for setting and resetting Master Password.
    
    Removed Krypto class as parent and made necessary refactoring.

--- src/Bix/Handler.java
+++ src/Bix/Handler.java
@@ -3,52 +3,110 @@ package Bix;
 import java.io.*;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
+import java.util.Properties;
 import java.util.Scanner;
+
 /**
  * This is an object class that handles reading and writing in .csv files
  * This class also encrypts and decrypts data from .csv files
  */
-class Handler extends Krypto {
+class Handler {
+    private Properties bix_properties;
+    private String IVRY_FILE; // stores the path to the location where the csv file is stored
+    private String HASH_FILE; // stores the path to the location where the master key hash file is stored
+    private String MASTER_PASSWORD_HASH; // stores the SHA256 hash of the master key
+    private boolean auth_success; // FALSE by default, turns TRUE if the user authentication is successful
+    private AES_FLAVOR aes_flavor;
 
-    String CSV_FILE; // stores the path to the location where the csv file is stored
-    String HASH_FILE; // stores the path to the location where the master key hash file is stored
+    private static Scanner input_scanner;
 
-    String master_key_hash; // stores the SHA256 hash of the master key
-    boolean auth_success; // FALSE by default, turns TRUE if the user authentication is successful
-
-    int AES_ALGORITHM;
-
-    static Scanner sc;
-
-    Handler(int algorithm) {
+    Handler() {
+        // Creating a Properties object to parse the config.properties file.
+        bix_properties = new Properties();
+        try {
+            bix_properties.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            System.out.println("ERROR: Missing config.properties file.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    /*
         // finding credentials.csv location
         try {
             // first line finds the location of .jar file, second line attaches
             // "credentials.csv" string to the parent path of .jar file
             // NOTE: it is assumed that the credentials.csv file is in the same folder as the .jar file
             File jarFile = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
-            CSV_FILE = jarFile.getParent() + File.separator + "credentials.csv";
+            IVRY_FILE = jarFile.getParent() + File.separator + "credentials.csv";
         } catch (Exception e) {e.printStackTrace();}
-
+*/
         HASH_FILE = "/mkhash.txt";
-        AES_ALGORITHM = algorithm;
+        aes_flavor = AES_FLAVOR.AES_128; // default AES flavor is 128 bit.
         auth_success = false;
-        sc = new Scanner(System.in);
-        verifyFileExists();
+        input_scanner = new Scanner(System.in);
+        //verifyFileExists();
 
         // retrieve the SHA256 hash of the master key stored in the credentials.csv file
-        try {
+        /*try {
             InputStreamReader isReader = new InputStreamReader(this.getClass().getResourceAsStream(HASH_FILE));
             BufferedReader br = new BufferedReader(isReader);
-            master_key_hash = br.readLine();
+            MASTER_PASSWORD_HASH = br.readLine();
             br.close();
-        } catch (Exception e) { e.printStackTrace(); }
+        } catch (Exception e) { e.printStackTrace(); }*/
     } //constructor
 
-    void verifyFileExists() { // method to check if the required csv and hash files exist
+    /**
+     *
+     * @return true if setup was successful or if it is already completed.
+     */
+    void setup(){
+        // If this is the first time opening Bix, do the initial setup.
+        if(Boolean.parseBoolean(bix_properties.getProperty("initial_setup_required"))){
+            if(setMasterPassword()) {
+                // If initial setup was successful, set the setup property to false
+                // to prevent setup procedure in the future.
+                bix_properties.setProperty("initial_setup_required", "false");
+            }
+            else {
+                System.out.println("\nBix Setup Failed.\n");
+                System.exit(2);
+            }
+        }
+    }// setup()
+
+    private boolean setMasterPassword(){
+        String first_input,second_input; // stores the user's input
+
+        // Get the new Master Password from user.
+        System.out.print("\n > Enter your new Master Password       : ");
+        first_input = input_scanner.next().trim();
+        System.out.print("\n > Enter your new Master Password again : ");
+        second_input = input_scanner.next().trim();
+
+        // Clearing the interface.
+        clearScreen();
+
+        // Checking that the first and second inputs match.
+        if(first_input.equals(second_input)){
+            System.out.println("\nNew Master Password has been set successfully.\n");
+            try {MASTER_PASSWORD_HASH = Krypto.getSHA256(first_input); }
+            catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
+            return true;
+        }
+        else{
+            System.out.println("\nFailed to set Master Password. Try again.\n");
+            return false;
+        }
+    }// setMasterPassword()
+
+    void setAESFlavor(AES_FLAVOR flavor){
+        this.aes_flavor = flavor;
+    }
+
+    private void verifyFileExists() { // method to check if the required csv and hash files exist
         boolean file_located = true; // turns false if one of the files is not found
 
-        File csv_file = new File(CSV_FILE);
+        File csv_file = new File(IVRY_FILE);
         if (csv_file.exists() == false) { // checking if csv file exists
             System.out.println("\nERROR: Failed to locate credentials.csv in filepath.");
             file_located = false;
@@ -62,11 +120,11 @@ class Handler extends Krypto {
 
     } //verifyFile()
 
-    void authenticate () { // method to authenticate user
+    void authenticate() { // method to authenticate user
         String input_key = getMasterKeyFromUser(false);
 
         try {
-            if (getSHA256(input_key).equals(master_key_hash)) { // comparing hash values
+            if (Krypto.getSHA256(input_key).equals(MASTER_PASSWORD_HASH)) { // comparing hash values
                 clearScreen();
                 System.out.println("\nAuthentication successful.");
                 auth_success = true;
@@ -84,16 +142,16 @@ class Handler extends Krypto {
         // get the master key from user
         if (newMK) { System.out.print("\n > Enter new Master Key: "); }
         else { System.out.print("\n > Enter Master Key: "); } // if setting a new master key
-        user_input = sc.nextLine().trim(); // reading user input
+        user_input = input_scanner.nextLine().trim(); // reading user input
 
-        clearScreen(); // clears all sensitive information from the screen
+        //clearScreen(); // clears all sensitive information from the screen
         return user_input;
 
     } // getMasterKeyFromUser()
 
     void printAccountNamesList () { // prints out a list of names of all accounts stored in credentials.csv
         if (auth_success) {
-            try (BufferedReader br = new BufferedReader(new FileReader(CSV_FILE))) {
+            try (BufferedReader br = new BufferedReader(new FileReader(IVRY_FILE))) {
                 String line;
                 System.out.println("\nStored Accounts: ");
                 while ((line = br.readLine()) != null) {
@@ -110,7 +168,7 @@ class Handler extends Krypto {
     boolean accountExists (String account_name){ // returns true if the account exists in credentials.csv
         if (auth_success) {
             account_name = account_name.toUpperCase(); // since the account names are stored in uppercase in csv file
-            try (BufferedReader br = new BufferedReader(new FileReader(CSV_FILE))) {
+            try (BufferedReader br = new BufferedReader(new FileReader(IVRY_FILE))) {
                 String line;
                 while ((line = br.readLine()) != null) {
                     String[] values = line.split(","); // split up values in the line and store in String array
@@ -141,7 +199,7 @@ class Handler extends Krypto {
     } // clearScreen()
 
     public void closeScanner () {
-        sc.close(); // closing scanner
+        input_scanner.close(); // closing scanner
     } // closeScanner()
 
     private void terminate () { // method to terminate the program
@@ -159,7 +217,7 @@ class Handler extends Krypto {
          */
 
         String[] values = null; // to store each value of a comma separated value line
-        try (BufferedReader br = new BufferedReader(new FileReader(CSV_FILE))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(IVRY_FILE))) {
             String line;
             while ((line = br.readLine()) != null) {
                 values = line.split(","); // split up values in the line and store in String array
@@ -181,14 +239,14 @@ class Handler extends Krypto {
 
         // comparing hash values of user entered password and hash stored in in csv file
         try {
-            if (!generateKeyAndGetHash(master_key, salt, AES_ALGORITHM).equals(values[4])) { // checking if hash values match
+            if (!Krypto.generateKeyAndGetHash(master_key, salt, aes_flavor.toInteger()).equals(values[4])) { // checking if hash values match
                 System.out.println("\nERROR: Password entered is incorrect.");
                 terminate(); // terminating Bix session
             }
         } catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
 
         // decrypting ciphertext
-        String plaintext = decrypt(ciphertext, master_key, salt, iv, AES_ALGORITHM);
+        String plaintext = Krypto.decrypt(ciphertext, master_key, salt, iv, aes_flavor.toInteger());
 
         // displaying credentials
         System.out.println("\nUsername: " + plaintext.substring(0, plaintext.indexOf(" ")));
@@ -212,7 +270,7 @@ class Handler extends Krypto {
 
         // verifying master_key
         try {
-            if (getSHA256(input_key).equals(master_key_hash)) { // comparing hash values
+            if (Krypto.getSHA256(input_key).equals(MASTER_PASSWORD_HASH)) { // comparing hash values
                 System.out.println("\nAuthentication successful.");
             } else {
                 System.out.println("\nERROR: Authentication failed. Incorrect key entered");
@@ -222,12 +280,12 @@ class Handler extends Krypto {
 
         // creating new csv line entry
         String plaintext = username + " " + password;
-        String new_csv_entry = account_name + "," + encrypt(plaintext, input_key, AES_ALGORITHM);
+        String new_csv_entry = account_name + "," + Krypto.encrypt(plaintext, input_key, aes_flavor.toInteger());
 
         // writing to csv file
         FileWriter csvWriter;
         try {
-            csvWriter = new FileWriter(CSV_FILE, true);
+            csvWriter = new FileWriter(IVRY_FILE, true);
             csvWriter.append(new_csv_entry);
             csvWriter.append("\n");
 
@@ -248,10 +306,10 @@ class Handler extends Krypto {
         // get the data from user
         do {
             System.out.print("\n > Enter " + data_name + ": ");
-            user_input = sc.nextLine().trim(); // reading user input
+            user_input = input_scanner.nextLine().trim(); // reading user input
 
             System.out.print("\n *> Confirm this " + data_name + "? [Y]/[n]: ");
-            if (sc.nextLine().trim().toLowerCase().charAt(0) == 'y') {
+            if (input_scanner.nextLine().trim().toLowerCase().charAt(0) == 'y') {
                 confirm = true;
             }
         } while (!confirm);
