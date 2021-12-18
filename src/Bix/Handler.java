c592c706cfd529ad1c5292cc02559cdb0a2ee386
Nadeem <62071086+CookieCrumbs19212@users.noreply.github.com>
2021-12-18 01:40:35 +0100

    Made all variables and methods in the Handler class as static.
    Added variable account_names[] to store the account names in memory for faster accessing.
    
    Redesigned how the Master Password is accepted from the user. Master Password is read using the console.readPassword() method which returns it as a char[].
    
    Added clearMasterPassword() method to clear the master password from the memory.
    
    Minor refactoring to incorporate the EXIT_CODES enum.
    
    Added JavaDoc for some methods.
    
    Added method loadAccountNames() to load account names to memory.
    
    Removed redundant closeScanner() method.

--- src/Bix/Handler.java
+++ src/Bix/Handler.java
@@ -11,14 +11,17 @@ import java.util.Scanner;
  * This class also encrypts and decrypts data from .csv files
  */
 class Handler {
-    private Properties bix_properties;
-    private String IVRY_FILE; // stores the path to the location where the csv file is stored
-    private String HASH_FILE; // stores the path to the location where the master key hash file is stored
-    private String MASTER_PASSWORD_HASH; // stores the SHA256 hash of the master key
-    private boolean auth_success; // FALSE by default, turns TRUE if the user authentication is successful
-    private AES_FLAVOR aes_flavor;
-
-    private static Scanner input_scanner;
+    private static Properties bix_properties;
+    private static String IVRY_FILE; // stores the path to the location where the csv file is stored
+    private static String HASH_FILE; // stores the path to the location where the master key hash file is stored
+    private static String MASTER_PASSWORD_HASH; // stores the SHA256 hash of the master key
+    private static boolean auth_success; // FALSE by default, turns TRUE if the user authentication is successful
+    private static AES_FLAVOR AES_flavor; // can be initialized to 128-bit, 192-bit or 256-bit.
+    private static int vault_size;
+    private static String account_names[]; // stores the stored account names.
+    private static Console console; // System console reference.
+    private static char[] master_password; // global char[] to store and access Master Password; must be cleared after use.
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     Handler() {
         // Creating a Properties object to parse the config.properties file.
@@ -28,9 +31,9 @@ class Handler {
         } catch (IOException e) {
             System.out.println("ERROR: Missing config.properties file.");
             e.printStackTrace();
-            System.exit(1);
+            System.exit(EXIT_CODES.MISSING_CONFIG_FILE.getExitCode());
         }
-    /*
+        /*
         // finding credentials.csv location
         try {
             // first line finds the location of .jar file, second line attaches
@@ -39,11 +42,13 @@ class Handler {
             File jarFile = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
             IVRY_FILE = jarFile.getParent() + File.separator + "credentials.csv";
         } catch (Exception e) {e.printStackTrace();}
-*/
+                */
         HASH_FILE = "/mkhash.txt";
-        aes_flavor = AES_FLAVOR.AES_128; // default AES flavor is 128 bit.
+        AES_flavor = AES_FLAVOR.AES_128; // default AES flavor is 128 bit.
         auth_success = false;
-        input_scanner = new Scanner(System.in);
+        vault_size = Integer.parseInt(bix_properties.getProperty("vault_size"));
+        account_names = new String[vault_size];
+        console = System.console(); // attaching variable to the system console.
         //verifyFileExists();
 
         // retrieve the SHA256 hash of the master key stored in the credentials.csv file
@@ -56,51 +61,61 @@ class Handler {
     } //constructor
 
     /**
-     *
-     * @return true if setup was successful or if it is already completed.
+     * Runs initial setup processes.
      */
-    void setup(){
+    static void setup(){
         // If this is the first time opening Bix, do the initial setup.
         if(Boolean.parseBoolean(bix_properties.getProperty("initial_setup_required"))){
             if(setMasterPassword()) {
-                // If initial setup was successful, set the setup property to false
-                // to prevent setup procedure in the future.
+                /* If initial setup was successful, set the "initial_setup_property" as false
+                   to prevent setup procedure from redundantly running again in the future. */
                 bix_properties.setProperty("initial_setup_required", "false");
             }
             else {
                 System.out.println("\nBix Setup Failed.\n");
-                System.exit(2);
+                terminateSession(EXIT_CODES.MISSING_CONFIG_FILE);
             }
         }
     }// setup()
 
-    private boolean setMasterPassword(){
+    /**
+     * Sets the Master Password for Bix. Standard Password setting process.
+     * @return
+     */
+    private static boolean setMasterPassword(){
         String first_input,second_input; // stores the user's input
 
         // Get the new Master Password from user.
-        System.out.print("\n > Enter your new Master Password       : ");
-        first_input = input_scanner.next().trim();
-        System.out.print("\n > Enter your new Master Password again : ");
-        second_input = input_scanner.next().trim();
+        System.out.print("\n > Enter your new Master Password (1st time) : ");
+        first_input = SCANNER.next().trim();
+        System.out.print("\n > Enter your new Master Password (2nd time) : ");
+        second_input = SCANNER.next().trim();
 
         // Clearing the interface.
         clearScreen();
 
         // Checking that the first and second inputs match.
         if(first_input.equals(second_input)){
-            System.out.println("\nNew Master Password has been set successfully.\n");
             try {MASTER_PASSWORD_HASH = Krypto.getSHA256(first_input); }
-            catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
+            catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+                return false;
+            }
             return true;
         }
         else{
-            System.out.println("\nFailed to set Master Password. Try again.\n");
+            System.out.println("\nThe Master Passwords you entered did not match. " +
+                                "Failed to set Master Password. Try again.\n");
             return false;
         }
     }// setMasterPassword()
 
-    void setAESFlavor(AES_FLAVOR flavor){
-        this.aes_flavor = flavor;
+    /**
+     * Set the AES flavor (128-bit, 192-bit or 256-bit).
+     * @param flavor The desired AES flavor to be set as the working flavor.
+     */
+    static void setAESFlavor(AES_FLAVOR flavor){
+        AES_flavor = flavor;
     }
 
     private void verifyFileExists() { // method to check if the required csv and hash files exist
@@ -116,41 +131,100 @@ class Handler {
             System.out.println("\nERROR: Failed to locate mkhash.txt in filepath.");
             file_located = false;
         }
-        if (!file_located) { terminate(); }
+        if (!file_located) { terminateSession(EXIT_CODES.MISSING_VAULT); }
 
     } //verifyFile()
 
-    void authenticate() { // method to authenticate user
-        String input_key = getMasterKeyFromUser(false);
+    /**
+     * Method to authenticate the user.
+     */
+    static void authenticateUser() {
+        // First, get the master password from the user.
+        getMasterPasswordFromUser();
 
         try {
-            if (Krypto.getSHA256(input_key).equals(MASTER_PASSWORD_HASH)) { // comparing hash values
+            // Successful authentication.
+            if (Krypto.getSHA256(new String(master_password)).equals(MASTER_PASSWORD_HASH)) { // comparing hash values
                 clearScreen();
                 System.out.println("\nAuthentication successful.");
                 auth_success = true;
-            } else {
-                System.out.println("\nAuthentication failed. Incorrect key entered.");
-                terminate();
+            }
+            // Failed authentication.
+            else {
+                System.out.println("\nAuthentication failed. Incorrect Master Password.");
+                terminateSession(EXIT_CODES.INCORRECT_MASTER_PASSWORD);
             }
         } catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
 
+        // Clearing the master Password from memory.
+        clearMasterPassword();
     } // authenticate()
 
-    private String getMasterKeyFromUser (boolean newMK){ // gets the master key from the user
-        String user_input; // stores the user's input
-
-        // get the master key from user
-        if (newMK) { System.out.print("\n > Enter new Master Key: "); }
-        else { System.out.print("\n > Enter Master Key: "); } // if setting a new master key
-        user_input = input_scanner.nextLine().trim(); // reading user input
-
-        //clearScreen(); // clears all sensitive information from the screen
-        return user_input;
-
+    /**
+     * Gets the Master Password from the user in a secure manner.
+     * Special precautions are taken to prevent the Master Password from being leaked.
+     *
+     * @return
+     */
+    private static void getMasterPasswordFromUser(){
+        System.out.print("\n > Enter Master Password: ");
+        master_password = console.readPassword(); // Getting the password from user.
     } // getMasterKeyFromUser()
 
-    void printAccountNamesList () { // prints out a list of names of all accounts stored in credentials.csv
-        if (auth_success) {
+    private static void clearMasterPassword(){
+        // loop to set every character in master_password to null character('\0').
+        for(char ch : master_password)
+            ch = '\0'; // setting to null character.
+    } // clearMasterPassword()
+
+    /**
+     * Retrieves Account login credentials requested by the user.
+     */
+    static void retrieveAccountLogin(){
+        System.out.println("\nRetrieve Account Login Credentials");
+
+        // Boolean to indicate if the account has been found.
+        boolean account_retrieved = false;
+        // prompt for searching by account name. Displays all account names when null.
+        String prompt = null;
+        do{
+
+            Handler.printAccountNames(prompt); // printing all account names
+
+            System.out.print("\n > Enter Account name (partial name is also accepted): ");
+            account_name = SCANNER.nextLine().trim().toUpperCase(); // reading account name
+
+            if (Handler.accountExists(account_name)) { //checking if account exists
+                Handler.getCredentialsFor(account_name); // get credentials for the account
+            }
+        }while();
+    } // retrieveAccountLogin()
+
+    /**
+     * Loads the account names from the credentials.csv file to memory for quicker access.
+     */
+    private static void loadAccountNames (){
+        try (BufferedReader br = new BufferedReader(new FileReader(IVRY_FILE))) {
+            int index = 0;
+            String line;
+            while ((line = br.readLine()) != null) {
+                account_names[index++] = line.substring(0, line.indexOf(',')); // split up values in the line and store in String array
+
+            } // while
+
+        } // try
+        catch (IOException e) { e.printStackTrace(); }
+    } // loadAccountNames()
+
+    /**
+     * Method to print stored account names.
+     * Prints the account names containing the String {@code prompt}.
+     *
+     * @param prompt Prompt to find account names containing a specific String.
+     *               Prints all account names when set to {@code null}.
+     */
+    static void printAccountNames (String prompt) { // prints out a list of names of all accounts stored in credentials.csv
+        if (prompt==null) {
             try (BufferedReader br = new BufferedReader(new FileReader(IVRY_FILE))) {
                 String line;
                 System.out.println("\nStored Accounts: ");
@@ -165,7 +239,7 @@ class Handler {
         } // auth_success
     } // printAccountNamesList()
 
-    boolean accountExists (String account_name){ // returns true if the account exists in credentials.csv
+    static boolean accountExists (String account_name){ // returns true if the account exists in credentials.csv
         if (auth_success) {
             account_name = account_name.toUpperCase(); // since the account names are stored in uppercase in csv file
             try (BufferedReader br = new BufferedReader(new FileReader(IVRY_FILE))) {
@@ -198,19 +272,28 @@ class Handler {
         } catch (Exception e) { e.printStackTrace(); }
     } // clearScreen()
 
-    public void closeScanner () {
-        input_scanner.close(); // closing scanner
-    } // closeScanner()
+    /**
+     * Terminates the current Bix session.
+     * @param exit_code
+     */
+    private static void terminateSession(EXIT_CODES exit_code) {
+        // Reassurance that the Master Password will always be cleared.
+        clearMasterPassword();
+        // Closing the Scanner stream.
+        SCANNER.close();
 
-    private void terminate () { // method to terminate the program
-        System.out.println("\nTerminating Bix session.");
-        closeScanner(); // closing scanner
-        System.exit(0); // terminating program
-    } // terminate()
+        // Clearing the console.
+        clearScreen();
+        // Displaying the exit message depending on the exit code.
+        System.out.printf("\n%s\nTerminating Bix session.",exit_code.getMessage());
+        // Terminating the session.
+        System.exit(exit_code.getExitCode());
+
+    } // terminateSession()
 
     /*-----------------------------------------------------------------------------------------*/
 
-    void getCredentialsFor (String account_name){
+    static void getCredentialsFor (String account_name){
         /** credentials.csv file format:
          *  account_name ,  ciphertext  ,     salt     ,     iv      , secret_key hash
          *    values[0]      values[1]      values[2]     values[3]       values[4]
@@ -230,7 +313,7 @@ class Handler {
         catch (IOException e) { e.printStackTrace(); }
 
         // getting master key from user
-        String master_key = getMasterKeyFromUser(false);
+        String master_key = getMasterPasswordFromUser();
 
         assert values != null;
         String ciphertext = values[1];
@@ -239,14 +322,14 @@ class Handler {
 
         // comparing hash values of user entered password and hash stored in in csv file
         try {
-            if (!Krypto.generateKeyAndGetHash(master_key, salt, aes_flavor.toInteger()).equals(values[4])) { // checking if hash values match
+            if (!Krypto.generateKeyAndGetHash(master_key, salt, AES_flavor.toInteger()).equals(values[4])) { // checking if hash values match
                 System.out.println("\nERROR: Password entered is incorrect.");
-                terminate(); // terminating Bix session
+                terminateSession(EXIT_CODES.INCORRECT_MASTER_PASSWORD); // terminating Bix session
             }
         } catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
 
         // decrypting ciphertext
-        String plaintext = Krypto.decrypt(ciphertext, master_key, salt, iv, aes_flavor.toInteger());
+        String plaintext = Krypto.decrypt(ciphertext, master_key, salt, iv, AES_flavor.toInteger());
 
         // displaying credentials
         System.out.println("\nUsername: " + plaintext.substring(0, plaintext.indexOf(" ")));
@@ -258,7 +341,7 @@ class Handler {
 
     /*-----------------------------------------------------------------------------------------*/
 
-    void createAccountLogin () {
+    static void addAccountLogin() {
         // get account information
         String account_name = getInput("account name").toUpperCase();
         String username = getInput("account username");
@@ -266,7 +349,7 @@ class Handler {
         clearScreen(); // clears all sensitive information from the screen
 
         // getting master_key from user
-        String input_key = getMasterKeyFromUser(false);
+        String input_key = getMasterPasswordFromUser(false);
 
         // verifying master_key
         try {
@@ -274,13 +357,13 @@ class Handler {
                 System.out.println("\nAuthentication successful.");
             } else {
                 System.out.println("\nERROR: Authentication failed. Incorrect key entered");
-                terminate();
+                terminateSession(EXIT_CODES.INCORRECT_MASTER_PASSWORD);
             }
         } catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
 
         // creating new csv line entry
         String plaintext = username + " " + password;
-        String new_csv_entry = account_name + "," + Krypto.encrypt(plaintext, input_key, aes_flavor.toInteger());
+        String new_csv_entry = account_name + "," + Krypto.encrypt(plaintext, input_key, AES_flavor.toInteger());
 
         // writing to csv file
         FileWriter csvWriter;
@@ -295,21 +378,21 @@ class Handler {
 
         // print success message and terminate
         System.out.println("Securely stored account credentials.");
-        terminate();
+        terminateSession(EXIT_CODES.SAFE_TERMINATION);
 
     } // createAccountLogin()
 
-    private String getInput (String data_name){
+    private static String getInput (String data_name){
         String user_input; // stores the user's input
         boolean confirm = false; // while loop control variable
 
         // get the data from user
         do {
             System.out.print("\n > Enter " + data_name + ": ");
-            user_input = input_scanner.nextLine().trim(); // reading user input
+            user_input = SCANNER.nextLine().trim(); // reading user input
 
             System.out.print("\n *> Confirm this " + data_name + "? [Y]/[n]: ");
-            if (input_scanner.nextLine().trim().toLowerCase().charAt(0) == 'y') {
+            if (SCANNER.nextLine().trim().toLowerCase().charAt(0) == 'y') {
                 confirm = true;
             }
         } while (!confirm);
