d5c7eacedc7c5af8665f14c248b958524f017e9d
Nadeem <62071086+CookieCrumbs19212@users.noreply.github.com>
2022-01-16 04:52:22 +0100

    Organized exit codes.

--- src/main/java/com/cookiecrumbs19212/bix/ExitCode.java
+++ src/main/java/com/cookiecrumbs19212/bix/ExitCode.java
@@ -1,23 +1,41 @@
 package com.cookiecrumbs19212.bix;
 
 public enum ExitCode {
-    // Safe Termination.
+    /*
+        How to Read Error Codes:
+        The 1st digit of the error codes indicate the general group the error belongs to.
+        e.g.: Error codes starting with '1' indicate a File/Resource error.
+
+        The 2nd digit further categorizes the error into subgroups.
+        e.g.: Error codes starting with '11' indicate that a File/Resource is not found.
+
+        Unknown errors in each group end with a '00'.
+        e.g.: The error code for an unknown File error is '100'.
+     */
+
+    // 0. Safe Termination.
     SAFE_TERMINATION(0, "Safe Termination."),
 
-    // Missing files or resources.
-    MISSING_CONFIG_FILE(100, "The config.properties file is missing."),
-    MISSING_VAULT_FILE(102, "The vault.bxdb file is missing."),
-    MISSING_CONFIG_PROPERTY(103, "Property Missing in Config File."),
-    CONSOLE_NOT_FOUND(202, "Could not find system console."),
+    // 1. File/Resource Errors.
+    UNKNOWN_RESOURCE_ERROR(100, "An unknown resource error has occurred."),
 
-    // Setup failures.
-    SETUP_FAILED_UNKNOWN(103,"Bix Setup Failed For Unknown Reason."),
-    MASTER_PASSWORD_SETUP_FAILED(104, "Master Password Setup Failed."),
+    PROPERTY_FILE_NOT_FOUND(110, "The config.properties file could not be found."),
+    VAULT_FILE_NOT_FOUND(111, "The vault.bxdb file could not be found."),
+    CONSOLE_NOT_FOUND(112, "Unable to find a system console associated with the current JVM."),
 
-    // Authentication failures.
-    AUTHENTICATION_FAILED(200, "User Authentication Failed."),
-    INCORRECT_MASTER_PASSWORD(201, "Authentication Failed. Incorrect Master Password.");
+    ERROR_ACCESSING_PROPERTY_FILE(120, "An error occurred while accessing the config.properties file."),
+    ERROR_ACCESSING_VAULT_FILE(121, "An error occurred while accessing the vault.bxdb file."),
 
+    // 2. Setup Errors.
+    UNKNOWN_SETUP_ERROR(200,"Bix setup process failed for an unknown reason."),
+
+    MASTER_PASSWORD_SETUP_FAILED(210, "Master Password setup failed."),
+
+    // 3. Authentication Failure.
+    UNKNOWN_AUTHENTICATION_ERROR(300, "An unknown error occurred during User Authentication."),
+
+    AUTHENTICATION_FAILED(310, "User Authentication Failed.");
+    //--------------------------------------------------------------------------------------------
 
     private final int exit_code;
     private final String message;
