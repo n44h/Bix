8e18903ea5673fc788bf2d22aca0f9d27310f517
CookieCrumbs19212 <developer19212@gmail.com>
2022-10-19 22:42:13 +0200

    Created exception AccountNotFoundException

---
+++ src/main/java/com/bix/exceptions/AccountNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bix.exceptions;
+
+public class AccountNotFoundException extends Exception {
+    public AccountNotFoundException(String accountName) {
+        super(String.format("Account \"%s\" does not exist.", accountName));
+    }
+}
