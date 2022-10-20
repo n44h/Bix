4bf2906ee5fc5dfb743dae95b0d8fe1c9ecf86ad
CookieCrumbs19212 <developer19212@gmail.com>
2022-10-20 14:40:37 +0200

    Added string representation for the AES flavors.
    Added toString() method.
    
    Added fromString() static method to convert strings to equivalent AESFlavor enum values.

--- src/main/java/com/bix/enums/AESFlavor.java
+++ src/main/java/com/bix/enums/AESFlavor.java
@@ -1,18 +1,38 @@
 package com.bix.enums;
 
 public enum AESFlavor {
-    AES_128(128),
-    AES_192(192),
-    AES_256(256);
+    AES_128(128, "AES-128"),
+    AES_192(192, "AES-192"),
+    AES_256(256, "AES-256");
 
-    private final int value;
+    private final int intValue;
+    private final String strValue;
 
-    AESFlavor(final int value) {
-        this.value = value;
+    AESFlavor(final int intValue, final String strValue) {
+        this.intValue = intValue;
+        this.strValue = strValue;
     }
 
     public int toInteger() {
-        return value;
+        return intValue;
+    }
+
+    public String toString() {
+        return strValue;
+    }
+
+    public static AESFlavor fromString(String value) {
+        switch(value) {
+            case "AES-128" -> {
+                return AES_128;
+            }
+            case "AES-192" -> {
+                return AES_192;
+            }
+            default -> {
+                return AES_256;
+            }
+        }
     }
 
 } // enum AESFlavor
