441da1b6557b66340a057351500d83db01a1a839
CookieCrumbs19212 <developer19212@gmail.com>
2023-02-10 21:37:14 +0100

    Modified status code numbering semantics. Status codes are now numbered contiguously.

--- src/main/java/com/bix/enums/StatusCode.java
+++ src/main/java/com/bix/enums/StatusCode.java
@@ -5,47 +5,32 @@ package com.bix.enums;
  * This includes safe terminations, runtime exceptions, timeouts, authentication status, misc. errors.
  */
 
-/*
- * How to Read Status Codes:
- * The 1st digit of the status code indicates a general group.
- * e.g.: Status codes starting with '1' indicate a File/Resource error.
- *
- * The 2nd digit further categorizes the status into a subgroup.
- * e.g.: Status codes starting with '11' indicate that a File/Resource is not found.
- *
- * Unknown errors in each group end with a '00'.
- * e.g.: The status code for an unknown File/Resource error is '100'.
- */
-
 public enum StatusCode {
 
-    // 0. Safe Termination.
+    // Safe Termination.
     SAFE_TERMINATION(0, "Safe Termination."),
 
-    // 1. File/Resource Errors.
-    UNKNOWN_RESOURCE_ERROR(100, "An unknown file/resource error has occurred."),
+    // Resource Not Found.
+    CONFIG_FILE_NOT_FOUND(1, "The config.properties file could not be found."),
+    VAULT_FILE_NOT_FOUND(2, "The vault.db file could not be found."),
+    CONSOLE_NOT_FOUND(3, "Unable to find a system console associated with the current JVM."),
 
-    // 1.1 Resource Not Found.
-    CONFIG_FILE_NOT_FOUND(110, "The config.properties file could not be found."),
-    VAULT_FILE_NOT_FOUND(111, "The vault.db file could not be found."),
-    CONSOLE_NOT_FOUND(112, "Unable to find a system console associated with the current JVM."),
+    // Resource Access Error.
+    ERROR_ACCESSING_CONFIG_FILE(4, "An error occurred while accessing the config.properties file."),
+    ERROR_ACCESSING_VAULT_FILE(5, "An error occurred while accessing the vault.db file."),
 
-    // 1.2 Resource Access Error.
-    ERROR_ACCESSING_CONFIG_FILE(120, "An error occurred while accessing the config.properties file."),
-    ERROR_ACCESSING_VAULT_FILE(121, "An error occurred while accessing the vault.db file."),
+    // Setup Failure.
+    MASTER_PASSWORD_SETUP_FAILED(6, "Master Password setup failed."),
 
-    // 2. Setup Errors.
-    UNKNOWN_SETUP_ERROR(200,"Bix setup process failed for an unknown reason."),
+    // Authentication Failure.
+    AUTHENTICATION_FAILED(7, "User Authentication Failed."),
 
-    MASTER_PASSWORD_SETUP_FAILED(210, "Master Password setup failed."),
+    // Idle Session Timeout.
+    IDLE_SESSION_TIMEOUT(8, "Bix session terminated due to inactivity."),
 
-    // 3. Authentication Failure.
-    UNKNOWN_AUTHENTICATION_ERROR(300, "An unknown error occurred during User Authentication."),
-
-    AUTHENTICATION_FAILED(310, "User Authentication Failed."),
-
-    // 4. Idle Session Timeout.
-    IDLE_SESSION_TIMEOUT(400, "Bix session terminated due to inactivity.");
+    // Unknown Errors.
+    UNKNOWN_RESOURCE_ERROR(126, "An unknown file/resource error has occurred."),
+    UNKNOWN_ERROR(127, "An unknown error occurred.");
     //--------------------------------------------------------------------------------------------
 
     private final int statusCode;
