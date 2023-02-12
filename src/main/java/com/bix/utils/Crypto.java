073536d568a1ec1f6031c1f44696cb3f8256f982
CookieCrumbs19212 <developer19212@gmail.com>
2023-02-12 18:16:51 +0100

    Enhanced cryptographic processes with Bouncy Castle provider
    Moved cipher algorithm string to Constants.java as CIPHER_ALGORITHM
    
    Added helper functions: byteToCharArray(), charToByteArray(), encode(), decode()
    
    Updated documentation
    
    Code clean up.

--- src/main/java/com/bix/utils/Crypto.java
+++ src/main/java/com/bix/utils/Crypto.java
@@ -1,15 +1,15 @@
 package com.bix.utils;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.StandardCharsets;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
+import java.security.*;
 import java.security.spec.KeySpec;
+
 import java.util.Arrays;
 import java.util.Base64;
 
@@ -20,13 +20,22 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
+// Get the cipher algorithm which contains the encryption system, encryption mode, and padding mode.
+import static com.bix.utils.Constants.CIPHER_ALGORITHM;
+
 /**
  * This class serves 2 functions:
  * 1. Generating SHA256 hashes
  * 2. Encrypting and decrypting data (with choice of AES flavors 128, 192, or 256)
  */
 
-final class Crypto {
+public final class Crypto {
+    private Crypto(){} // Enforce non-instantiability of this class.
+
+    static {
+        // Add the Bouncy Castle provider.
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     // SecureRandom object for SALT and Initialization Vector (IV) generation.
     private static final SecureRandom RANDOM = new SecureRandom();
@@ -38,21 +47,20 @@ final class Crypto {
      *
      * @param input String input to hash
      *
-     * @return The {@code SHA256} hash of the input as a hexadecimal String
+     * @return SHA256 hash as a hexadecimal String
      */
-    static String getSHA256Hash(char[] input) {
+    public static String getSHA256Hash(char[] input) {
         // Generating MessageDigest object initialized with the SHA-256 algorithm.
         MessageDigest md;
         try {
-            md = MessageDigest.getInstance("SHA-256");
+            md = MessageDigest.getInstance("SHA-256", "BC");
         }
-        catch (NoSuchAlgorithmException e) {
+        catch (Exception e) {
             throw new RuntimeException(e);
         }
 
         // Converting char[] to byte[].
-        ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(CharBuffer.wrap(input));
-        byte[] inputAsBytes = Arrays.copyOf(byteBuffer.array(), byteBuffer.limit());
+        byte[] inputAsBytes = charToByteArray(input);
 
         /* md.digest() method generates the message digest of the input and returns a byte array.
          * The byte array is then converted into a hexadecimal String using toHexString(). */
@@ -60,72 +68,125 @@ final class Crypto {
     }
 
     /**
-     * Converts byte arrays to hexadecimal Strings
+     * Gets the hash of the Secret Key object
      *
-     * @param input as a byte array
+     * @param secretKey the secret key to hash
      *
-     * @return a hexadecimal String
+     * @return the SHA256 hash of the secret key as a hexadecimal String
      */
-    private static String toHexString(byte[] input) {
-        // Convert byte array into signum representation
-        BigInteger number = new BigInteger(1, input);
+    public static String getKeyHash(SecretKey secretKey) {
+        // Convert SecretKey object to byte array.
+        byte[] byteArray = secretKey.getEncoded();
 
-        // Convert message digest into hex value
-        StringBuilder hexString = new StringBuilder(number.toString(16));
+        // Converting byte[] to char[], then return the SHA256 hash of the char[].
+        return getSHA256Hash(byteToCharArray(byteArray));
+    }
 
-        // Pad with leading zeros
-        while (hexString.length() < 64) { hexString.insert(0, '0'); }
+    /**
+     * Compares secret key hash generated from {@code masterPassword} and {@code salt} to the {@code target hash}
+     *
+     * @param masterPassword the master password
+     * @param salt the salt as a String
+     * @param algorithm AES flavor: 128, 192, or 256
+     *
+     * @return true if the hash of the generated secret key matches the target hash
+     */
+    public static boolean authenticateSecretKey(char[] masterPassword, String salt, int algorithm, String targetHash) {
+        // Generate the secret key.
+        SecretKey secretKey = getSecretKey(masterPassword, decode(salt), algorithm);
 
-        return hexString.toString();
+        // Return true iff secret key is not null, and it is equal to the target hash.
+        return secretKey != null && getKeyHash(secretKey).equals(targetHash);
     }
 
 
-    // AES Encryption
+    // AES Encryption/Decryption
     /**
      * Encrypts plaintext using AES algorithm
      *
      * @param plaintext the String to be encrypted
-     *
      * @param password the password, along with a randomly generated salt, will be used to generate the secret key
      *                 for encrypting the plaintext
+     * @param algorithm AES flavor: 128, 192, or 256
      *
-     * @param flavor flavor of AES: 128, 192, or 256; Should be of type {@code int}
-     *
-     * @return a String containing comma separated values with the format: "CIPHERTEXT,SALT,IV,KEY_HASH"
+     * @return a String[] containing [CIPHERTEXT (base64), SALT (base64), IV (base64), SECRET_KEY_HASH]
      */
-    static String encrypt(char[] plaintext, char[] password, int flavor) {
+    public static String[] encrypt(char[] plaintext, char[] password, int algorithm) {
         try{
             // Generate random salt.
-            byte[] salt = generateRandomSalt();
+            var salt = generateRandomSalt();
 
             // Generate random initialization vector (IV).
-            IvParameterSpec iv = generateRandomIV();
+            var iv = generateRandomIV();
 
             // Generate secret key.
-            SecretKey secretKey = getSecretKey(password, salt, flavor);
+            var secretKey = getSecretKey(password, salt, algorithm);
 
             // Initializing cipher for AES in CBC mode using PKCS5 padding.
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            var cipher = Cipher.getInstance(CIPHER_ALGORITHM, "BC");
+
+            // Initialize the cipher in encrypt mode with the secret key and IV.
             cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
 
             // Convert the plaintext from char[] to byte[].
-            ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(CharBuffer.wrap(plaintext));
-            byte[] plaintextBytes = Arrays.copyOf(byteBuffer.array(), byteBuffer.limit());
+            byte[] plaintextBytes = charToByteArray(plaintext);
 
-            // Encrypting the plaintext and then encoding to Base64 String.
-            String ciphertext = Base64.getEncoder().encodeToString(cipher.doFinal(plaintextBytes));
+            // Encrypting the plaintext.
+            byte[] ciphertext = cipher.doFinal(plaintextBytes);
 
-            // Return comma separated values: "ciphertext(base64), salt(base64), iv(base64), hash of secretKey".
-            return ( ciphertext + "," + Base64.getEncoder().encodeToString(salt) + "," +
-                    Base64.getEncoder().encodeToString(iv.getIV()) + "," + getKeyHash(secretKey));
+            // Return a String[] containing [ciphertext(base64), salt(base64), iv(base64), hash of secretKey].
+            return new String[] {
+                    encode(ciphertext),
+                    encode(salt),
+                    encode(iv.getIV()),
+                    getKeyHash(secretKey)
+            };
         }
         catch(Exception e) {
-            System.out.println("\n\nEncryption Process Failure");
-            e.printStackTrace();
+            throw new RuntimeException();
         }
-        return "0";
     }
 
+    /**
+     * Decrypts the AES-encrypted ciphertext
+     *
+     * @param ciphertext the ciphertext to decrypt
+     * @param password the master password
+     * @param salt salt used during encryption
+     * @param iv initialization vector used during encryption
+     * @param algorithm AES flavor: 128, 192 or 256.
+     *
+     * @return the decrypted plaintext as a String
+     */
+    public static char[] decrypt(char[] password, String ciphertext, String salt, String iv, int algorithm) {
+
+        // Generate secret key object.
+        var secretKey = getSecretKey(password, decode(salt), algorithm);
+
+        // Initialize IvParameterSpec object.
+        var ivSpec = new IvParameterSpec(decode(iv));
+
+        // Decrypting ciphertext.
+        char[] plaintext;
+        try {
+            // Initialize Cipher object.
+            var cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+
+            // Decode the ciphertext from Base64 String to byte[] then decrypt the ciphertext.
+            byte[] plaintextByteArray = cipher.doFinal(decode(ciphertext));
+
+            // Converting plaintext from byte[] to char[].
+            plaintext = byteToCharArray(plaintextByteArray);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return plaintext;
+    }
+
+
+    // Helper Functions
     /**
      * Generates a random salt
      *
@@ -152,10 +213,8 @@ final class Crypto {
      * Generates the Secret Key
      *
      * @param password the master password
-     *
      * @param salt randomly generated salt
-     *
-     * @param algorithm AES flavor: 128, 192, or 256 as int
+     * @param algorithm AES flavor: 128, 192, or 256
      *
      * @return {@code SecretKey} object
      */
@@ -163,7 +222,7 @@ final class Crypto {
         SecretKey secretKey;
         try {
             // Create an instance of SecretKeyFactory with Password-Based Key Derivation Function 2 (PBKDF2).
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256", "BC");
 
             // Create a Key Specifications object.
             KeySpec spec = new PBEKeySpec(password, salt, 65536, algorithm);
@@ -171,95 +230,76 @@ final class Crypto {
             // Generate the secret key
             secretKey = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
         }
-        catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+        catch (Exception e) {
             throw new RuntimeException(e);
         }
-
         return secretKey;
     }
 
     /**
-     * Gets the hash of the Secret Key object
+     * Converts byte arrays to hexadecimal Strings
      *
-     * @param secretKey the secret key to hash
+     * @param input a byte array
      *
-     * @return the SHA256 hash of the secret key as a hexadecimal String
+     * @return a hexadecimal String
      */
-    static String getKeyHash(SecretKey secretKey) {
-        // Convert SecretKey object to byte array.
-        byte[] byteArray = secretKey.getEncoded();
+    private static String toHexString(byte[] input) {
+        // Convert byte array into signum representation.
+        var number = new BigInteger(1, input);
 
-        // Converting byte[] to char[].
+        // Convert message digest into hex value.
+        var hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros.
+        while (hexString.length() < 64) { hexString.insert(0, '0'); }
+
+        return hexString.toString();
+    }
+
+    /**
+     * Converts byte array to char array.
+     *
+     * @param byteArray byte array to convert to char array
+     *
+     * @return char[]
+     */
+    private static char[] byteToCharArray(byte[] byteArray) {
         final CharBuffer charBuffer = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(byteArray));
-        char[] hex = Arrays.copyOf(charBuffer.array(), charBuffer.limit());
-
-        // Return the SHA256 hash of the hex string.
-        return getSHA256Hash(hex);
+        return Arrays.copyOf(charBuffer.array(), charBuffer.limit());
     }
 
     /**
-     * Compares secret key hash generated from {@code masterPassword} and {@code salt} to the {@code target hash}
+     * Converts char array to byte array.
      *
-     * @param masterPassword the master password
+     * @param charArray char array to convert to byte array
      *
-     * @param salt the salt as a String
-     *
-     * @param algorithm AES flavor as an {@code int} 128, 192, or 256
-     *
-     * @return true if the hash of the generated secret key matches the target hash
+     * @return byte[]
      */
-    static boolean authenticateSecretKey(char[] masterPassword, String salt, int algorithm, String targetHash) {
-        // Generate the secret key.
-        SecretKey secretKey = getSecretKey(masterPassword, Base64.getDecoder().decode(salt), algorithm);
-
-        // Return true iff secret key is not null, and it is equal to the target hash.
-        return secretKey != null && getKeyHash(secretKey).equals(targetHash);
+    private static byte[] charToByteArray(char[] charArray) {
+        ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(CharBuffer.wrap(charArray));
+        return Arrays.copyOf(byteBuffer.array(), byteBuffer.limit());
     }
 
-
-    // AES Decryption
     /**
-     * Decrypts the AES-encrypted ciphertext
+     * Encodes byte array to Base64 String.
      *
-     * @param ciphertext the ciphertext to decrypt
+     * @param input byte[] input
      *
-     * @param password the master password
-     *
-     * @param salt salt used during encryption
-     *
-     * @param iv initialization vector used during encryption
-     *
-     * @param algorithm the AES flavor as an {@code int}, 128, 192 or 256.
-     *
-     * @return the decrypted plaintext as a String
+     * @return Base64 encoded String
      */
-    static char[] decrypt(char[] password, String ciphertext, String salt, String iv, int algorithm) {
+    private static String encode(byte[] input) {
+        return Base64.getEncoder().encodeToString(input);
+    }
 
-        // Generate secret key object.
-        SecretKey secretKey = getSecretKey(password, Base64.getDecoder().decode(salt), algorithm);
-
-        // Initialize IvParameterSpec object.
-        IvParameterSpec ivSpec = new IvParameterSpec(Base64.getDecoder().decode(iv));
-
-        // Decrypting ciphertext.
-        char[] plaintext;
-        try {
-            // Initialize Cipher object.
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
-
-            // Decrypting the ciphertext then converting the resulting byte array to a String.
-            byte[] plaintextByteArray = cipher.doFinal(Base64.getDecoder().decode(ciphertext));
-
-            // Converting byte[] to char[].
-            final CharBuffer charBuffer = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(plaintextByteArray));
-            plaintext = Arrays.copyOf(charBuffer.array(), charBuffer.limit());
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return plaintext;
+    /**
+     * Decodes Base64 encoded String to byte array.
+     *
+     * @param input Base64 encoded String
+     *
+     * @return byte[]
+     */
+    private static byte[] decode(String input) {
+        return Base64.getDecoder().decode(input);
     }
 
 } // class Crypto
