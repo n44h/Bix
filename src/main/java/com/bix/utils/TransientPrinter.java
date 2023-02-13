56bf2dd52611489ed8d7f3fc0c7460b034b0438a
CookieCrumbs19212 <developer19212@gmail.com>
2023-02-14 00:31:58 +0100

    Fixed bug where screen was not cleared after timer ends.

--- src/main/java/com/bix/utils/TransientPrinter.java
+++ src/main/java/com/bix/utils/TransientPrinter.java
@@ -1,69 +1,62 @@
 package com.bix.utils;
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Arrays;
 
-import static com.bix.utils.Controller.clearScreen;
-import static com.bix.utils.Controller.clearCharArrayFromMemory;
+import static com.bix.utils.Utils.clearScreen;
 
 public class TransientPrinter {
-    /**
-     * keyPressListener class that is listens for the Enter key press and clears the screen.
-     */
-    private static class KeyPressListener extends Thread {
-        @Override
-        public void run() {
-            final Scanner scanner = new Scanner(System.in);
-
-            // If a return key (enter key) press is registered, clear the screen.
-            System.out.println("\nPress the Enter key to clear the screen");
-            // Reads for an Enter key press.
-            scanner.nextLine();
-
-            // Cancel the Display Timer.
-            DISPLAY_TIMER.cancel();
-
-            // Clear credentials from the screen.
-            clearScreen();
-        }
-    }
-
-    private static final KeyPressListener LISTENER = new KeyPressListener();
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     // DISPLAY_DURATION dictates how long to display the values
     private static int DISPLAY_DURATION;
 
+    private static boolean CLEAR_SCREEN_TASK_COMPLETED;
+
     /* Timer object starts a background thread.
      * TimerTask is a task that can be scheduled and linked to the Timer object.
-     * Our goal for this class is to terminate the current Bix session if the
-     * session has been idle for a certain period of time (determined by idle_timeout).
      *
-     * The task in our case is terminating the current Bix session.
-     * And so, the TimerTask object calls the Controller.terminateSession().
+     * The purpose of this class is print an output to the terminal and, after a
+     * pre-determined amount of time, clear the terminal (determined by DISPLAY_DURATION).
+     *
+     * The terminal can also be cleared earlier if the user hits the enter key.
+     *
+     * The "timer task" in this case is clearing the terminal.
      */
-    private static final Timer DISPLAY_TIMER = new Timer();
-    private static final TimerTask CLEAR_SCREEN_TASK = new TimerTask() {
+    private final Timer DISPLAY_TIMER = new Timer();
+
+    // Creating Anonymous Inner class based on TimeTask class.
+    private final TimerTask CLEAR_SCREEN_TASK = new TimerTask() {
         @Override
         public void run() {
-            // Clear the credentials form the screen.
-            clearScreen();
+            // Perform the clear screen task if it has not been completed yet.
+            if (!CLEAR_SCREEN_TASK_COMPLETED) {
+                // Clear the credentials form the screen.
+                clearScreen();
 
-            // Kill the keyPressListener thread.
-            LISTENER.interrupt();
+                // Indicate task completion.
+                CLEAR_SCREEN_TASK_COMPLETED = true;
+            }
+
+            // Kill the thread.
+            DISPLAY_TIMER.cancel();
         }
     };
 
+
     /**
-     * Sets the Credential Display Duration. Default duration is 120 seconds (2 minutes).
-     * As a reasonable security measure, the new timeout cannot exceed 600 seconds (10 minutes).
-     * New timeout can also not be less than 1 second.
+     * TransientPrinter constructor.
      *
-     * @param duration credential display duration in seconds
+     * @param displayDuration duration the output should be displayed before clearing the terminal
      */
-    public static void setDisplayDuration(int duration) {
-        if(duration < 1)
-            DISPLAY_DURATION = 1;
-        else
-            DISPLAY_DURATION = Math.min(duration, 10 * 60);
+    public TransientPrinter(int displayDuration) {
+        // Display duration should be between 1 and 600 seconds (inclusive bounds).
+        DISPLAY_DURATION = Math.min(Math.max(displayDuration, 1), 10 * 60);
+
+        // Initialize to false, set to true when either of the task execution conditions are triggered.
+        CLEAR_SCREEN_TASK_COMPLETED = false;
     }
 
     /**
@@ -78,42 +71,65 @@ public class TransientPrinter {
      * {@code idle_session_timer.schedule(terminate_idle_session_task, idle_timeout)}.
      * </p>
      */
-    private static void startSessionMonitor() {
+    private void startDisplayTimer() {
         // Cancel any existing display timers.
-        DISPLAY_TIMER.cancel();
+        try { DISPLAY_TIMER.cancel(); }
+        catch (IllegalStateException ignored) {}
 
         // Scheduling the clear screen task with the display timer.
         // Multiply DISPLAY_DURATION by 1000 because Timer.schedule() takes arguments in milliseconds.
         DISPLAY_TIMER.schedule(CLEAR_SCREEN_TASK, DISPLAY_DURATION * 1000L);
-
-        // Start the key press listener thread to read Enter key presses, if an enter key is pressed, the
-        // display timer is cancelled and the screen is cleared immediately.
-        LISTENER.start();
     }
 
-    static void display(String arg) {
+    public void display(String arg) {
         // Display the argument.
         System.out.println(arg);
 
-        // Set arg to null.
-        arg = null;
+        // Start the timer for the clear screen task.
+        startDisplayTimer();
 
-        // Start the display timer.
-        startSessionMonitor();
+        // If an enter key (a.k.a. return key) press is registered, clear the screen.
+        System.out.println("\nPress the Enter key to clear the screen");
+        SCANNER.nextLine(); // Reads for an Enter key press.
+
+        // Perform clear screen task if it has not been completed already.
+        if(!CLEAR_SCREEN_TASK_COMPLETED) {
+            // Clear credentials from the screen.
+            clearScreen();
+
+            // Indicate task completion.
+            CLEAR_SCREEN_TASK_COMPLETED = true;
+        }
+
+        // Cancel the Display Timer.
+        DISPLAY_TIMER.cancel();
+
     }
 
-    static void displayCredentials(char[] username, char[] password, String associatedEmail) {
+    public void displayCredentials(char[] username, char[] password, String associatedEmail) {
         // Print credentials to Terminal.
         System.out.printf("\nUsername         : %s", Arrays.toString(username));
         System.out.printf("\nPassword         : %s", Arrays.toString(password));
         System.out.printf("\nAssociated email : %s", associatedEmail == null ? "nil" : associatedEmail);
 
-        // Clear credentials from memory.
-        clearCharArrayFromMemory(username);
-        clearCharArrayFromMemory(password);
-        associatedEmail = null;
-
         // Start the display timer.
-        startSessionMonitor();
+        startDisplayTimer();
+
+        // If an enter key (a.k.a. return key) press is registered, clear the screen.
+        System.out.println("\nPress the Enter key to clear the screen");
+        SCANNER.nextLine(); // Reads for an Enter key press.
+
+        // Perform clear screen task if it has not been completed already.
+        if(!CLEAR_SCREEN_TASK_COMPLETED) {
+            // Clear credentials from the screen.
+            clearScreen();
+
+            // Indicate task completion.
+            CLEAR_SCREEN_TASK_COMPLETED = true;
+        }
+
+        // Cancel the Display Timer.
+        DISPLAY_TIMER.cancel();
     }
-}
+
+} // class TransientPrinter
