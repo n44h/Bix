81759e7e6daf7bfe197000a26d6e6b2a916f3a1a
CookieCrumbs19212 <developer19212@gmail.com>
2023-02-14 00:25:31 +0100

    Updated status message
    Removed logLevels.

--- src/main/java/com/bix/enums/StatusCode.java
+++ src/main/java/com/bix/enums/StatusCode.java
@@ -8,38 +8,36 @@ package com.bix.enums;
 public enum StatusCode {
 
     // Safe Termination.
-    SAFE_TERMINATION(0, "Safe Termination.", "info"),
+    SAFE_TERMINATION(0, "Session safely terminated."),
 
     // Resource Not Found.
-    CONFIG_FILE_NOT_FOUND(1, "The config.properties file could not be found.", "error"),
-    VAULT_FILE_NOT_FOUND(2, "The vault.db file could not be found.", "error"),
-    CONSOLE_NOT_FOUND(3, "Unable to find a system console associated with the current JVM.", "error"),
+    CONFIG_FILE_NOT_FOUND(1, "The config.properties file could not be found."),
+    VAULT_FILE_NOT_FOUND(2, "The vault.db file could not be found."),
+    CONSOLE_NOT_FOUND(3, "Unable to find a system console associated with the current JVM."),
 
     // Opening GitHub Page Error.
-    ERROR_OPENING_GITHUB_PAGE(4, "An error occurred while accessing the config.properties file.", "warn"),
-    INVALID_GITHUB_URL_SYNTAX(5, "An error occurred while accessing the vault.db file.", "warn"),
+    ERROR_OPENING_GITHUB_PAGE(4, "An error occurred while accessing the config.properties file."),
+    INVALID_GITHUB_URL_SYNTAX(5, "An error occurred while accessing the vault.db file."),
 
     // Setup Failure.
-    MASTER_PASSWORD_SETUP_FAILED(6, "Master Password setup failed.", "error"),
+    MASTER_PASSWORD_SETUP_FAILED(6, "Master Password setup failed."),
 
     // Authentication Failure.
-    AUTHENTICATION_FAILED(7, "User Authentication Failed.", "error"),
+    AUTHENTICATION_FAILED(7, "User Authentication Failed."),
 
     // Idle Session Timeout.
-    IDLE_SESSION_TIMEOUT(8, "Bix session terminated due to inactivity.", "warn"),
+    IDLE_SESSION_TIMEOUT(8, "Bix session terminated due to inactivity."),
 
     // Unknown Errors.
-    UNKNOWN_ERROR(127, "An unknown error occurred.", "error");
+    UNKNOWN_ERROR(127, "An unknown error occurred.");
     //--------------------------------------------------------------------------------------------
 
     public final int code;
     public final String message;
-    public final String logLevel;
 
-    StatusCode(final int code, final String message, final String logLevel) {
+    StatusCode(final int code, final String message) {
         this.code = code;
         this.message = message;
-        this.logLevel = logLevel;
     }
 
 } // enum StatusCode
